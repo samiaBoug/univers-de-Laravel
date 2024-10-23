73d1089a868510635eafadf4ce1c7236ac51372a
Samiaboug <bougtoubsamia@gmail.com>
2024-10-23 10:50:07 +0100

    tutorials

---
+++ tutorials/app/Http/Controllers/ContactController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail ;
+
+class ContactController extends Controller
+{
+    public function create(){
+        return view('contact') ;
+    }
+
+    public function store(request $request){
+        //valider les données 
+        $data = $request ->validate([
+            "name" => 'required' ,
+            "email" => 'required|email',
+            "message" => 'required' ,
+        ]);
+
+        //envoi de l'email
+        Mail::send('emails.contact' , $data , function($message)use ($data){
+            $message -> to('bougtoub.samia.solicode@gmail.com')
+            -> subject('nouveau message de contact') ;
+        });
+
+        // Rediriger après l'envoi
+        return redirect('/contact')->with('success', 'Votre message a bien été envoyé.');
+    }
+}
