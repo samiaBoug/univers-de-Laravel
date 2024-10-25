ebb84041b54e9de212b98879bbbf3bfc31438b09
Samiaboug <bougtoubsamia@gmail.com>
2024-10-25 10:07:02 +0100

    live coding

---
+++ liveCoding/resources/views/contact.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+<form action="">
+    <label for="name">Nom :</label>
+    <input type="text" name="name">
+    <button type="submit">Envoyer</button>
+</form>
+@endsection
---
+++ liveCoding/resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+
+<h1>Bougtoub Samia</h1>
+
+@endsection
