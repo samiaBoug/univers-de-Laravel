681d95790c3e01176dd166fe01a7649d8be1cecd
AbderrafieElmrabet <abderafieelmrabet@gmail.com>
2024-10-25 13:56:48 +0100

    home

--- miniProjet/routes/web.php
+++ miniProjet/routes/web.php
@@ -2,15 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\AboutController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 
-Route::get('/about', function(){
-    return view('about') ;
-}) ;
+Route::get('/about', [AboutController::class, 'about']);
 
 Route::get('/contact', [ContactController::class, 'create']);
 Route::post('/contact', [ContactController::class, 'store']);
